7588ce9303186f6d85657a50898f947c133a10c4
volodymyr-kushnir <volodymyr.kushnir@me.com>
2019-02-14 12:58:19 +0200

    Skip login

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      token: null,
+      token: true,
       lesson: null,
       hometask: null,
       homeworkAssigned: false

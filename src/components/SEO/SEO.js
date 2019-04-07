1320324e12178a58ecebd318b7fe4b867c4df55d
volodymyr <volodymyr.kushnir@me.com>
2019-04-07 14:27:22 +0300

    Fix issues

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -1,82 +1,100 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-import { useStaticQuery } from "gatsby";
-import { getSiteMetadata } from "../../queries";
+import { StaticQuery, graphql } from "gatsby";
+
+export const getSiteMetadata = graphql`
+  query getSiteMetadata {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`;
 
 const SEO = ({ description, lang, meta, keywords, title }) => {
-  const { site } = useStaticQuery(getSiteMetadata);
-  const metaDescription = description || site.siteMetadata.description;
   return (
-    <Helmet
-      htmlAttributes={{
-        lang
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription
-        },
-        {
-          property: `og:title`,
-          content: title
-        },
-        {
-          property: `og:description`,
-          content: metaDescription
-        },
-        {
-          property: `og:type`,
-          content: `website`
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author
-        },
-        {
-          name: `twitter:title`,
-          content: title
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription
-        }
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `)
+    <StaticQuery
+      query={getSiteMetadata}
+      render={data => {
+        const metaDescription =
+          description || data.site.siteMetadata.description;
+        return (
+          <Helmet
+            htmlAttributes={{
+              lang
+            }}
+            title={title}
+            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+            meta={[
+              {
+                name: "description",
+                content: metaDescription
+              },
+              {
+                property: "og:title",
+                content: title
+              },
+              {
+                property: "og:description",
+                content: metaDescription
+              },
+              {
+                property: "og:type",
+                content: "website"
+              },
+              {
+                name: "twitter:card",
+                content: "summary"
+              },
+              {
+                name: "twitter:creator",
+                content: data.site.siteMetadata.author
+              },
+              {
+                name: "twitter:title",
+                content: title
+              },
+              {
+                name: "twitter:description",
+                content: metaDescription
               }
-            : []
-        )
-        .concat(meta)}
-    >
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/spectre.css/dist/spectre.min.css"
-      />
-    </Helmet>
+            ]
+              .concat(
+                keywords.length > 0
+                  ? {
+                      name: "keywords",
+                      content: keywords.join(", ")
+                    }
+                  : []
+              )
+              .concat(meta)}
+          >
+            <link
+              rel="stylesheet"
+              href="https://unpkg.com/spectre.css/dist/spectre.min.css"
+            />
+          </Helmet>
+        );
+      }}
+    />
   );
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: "en",
   meta: [],
   keywords: [],
-  description: ``
+  description: ""
 };
 
 SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
+  meta: PropTypes.array,
   keywords: PropTypes.arrayOf(PropTypes.string),
   title: PropTypes.string.isRequired
 };
--- src/queries/index.js
+++
@@ -1,1 +0,0 @@
-export { default as getSiteMetadata } from "./getSiteMetadata";

b9602b4a1dc36affd56c5a02c40be562b5ccab91
volodymyr-kushnir <volodymyr.kushnir@me.com>
2019-05-10 00:05:25 +0300

    Refactor <Footer /> — add some emojis

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,17 @@ import React from "react";
 
 const Footer = ({ author }) => (
   <footer className="hero hero-sm">
-    <div className="text-gray">
-      Copyright © 2019 <strong>{author || "Contributors"}</strong>. All rights reserved.
+    <div class="navbar">
+      <section class="navbar-section">
+        <div className="text-gray">
+          Copyright © 2019 <strong>{author || "Contributors"}</strong>. All rights reserved.
+        </div>
+      </section>
+      <section class="navbar-section">
+        <div className="text-gray">
+          Made with 🧠, 🤲, 💻, and ❤️.
+        </div>
+      </section>
     </div>
   </footer>
 );

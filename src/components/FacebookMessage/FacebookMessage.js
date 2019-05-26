3f5dad932de7e041c27aa818fe7d79c47e778f63
volodymyr-kushnir <volodymyr.kushnir@me.com>
2019-05-26 16:10:38 +0200

    Remove react-reactions

--- src/components/FacebookMessage/FacebookMessage.js
+++ src/components/FacebookMessage/FacebookMessage.js
@@ -1,17 +1,15 @@
 import React from "react";
 // import PropTypes from "prop-types";
 import classNames from "classnames";
-import { FacebookSelector } from "react-reactions";
 import "./FacebookMessage.css";
 
-const FacebookMessage = ({ children, small, subtle, withReactions }) => {
+const FacebookMessage = ({ children, small, subtle }) => {
   return (
     <div className={classNames("bubble", {
       "small": small,
       "subtle": subtle
     })}>
       {children}
-      {withReactions && <FacebookSelector />}
     </div>
   );
 }
